5c69c8dd513f8479a8124308f842c445daa75be0
Hyunchul Kim <hynchl.kim@gmail.com>
2020-11-09 01:38:27 +0900

    Updates source address

--- distribusi/distribusi/page_template.py
+++ distribusi/distribusi/page_template.py
@@ -5,22 +5,22 @@ html_head = """
     <!-- Generated with distribusi https://git.vvvvvvaria.org/varia/distribusi -->
     <meta name="generator" content="distribusi" />
     <meta http-equiv="content-type" content="text/html; charset=utf-8"> 
-    <link rel="stylesheet" type="text/css" href="https://frog.dianaband.info/src/style/common.css" />
+    <link rel="stylesheet" type="text/css" href="https://frog2.dianaband.info/src/style/common.css" />
     %s
   </head>
   <body>
 <div>
 <div id="menu">
     <div id="logo_wrapper" class="svg_wrapper">
-      <img id="logo_foh"src="https://frog.dianaband.info/src/img/logo.svg" alt="환대의 조각 로고">
+      <img id="logo_foh"src="https://frog2.dianaband.info/src/img/logo.svg" alt="환대의 조각 로고">
     </div>
 
     <div id="about_icon_wrapper" class="svg_wrapper">
-      <img id="about_icon"src="https://frog.dianaband.info/src/img/icon_about.svg" alt="각진 꽃 모양 어바웃 버튼">
+      <img id="about_icon"src="https://frog2.dianaband.info/src/img/icon_about.svg" alt="각진 꽃 모양 어바웃 버튼">
     </div>
 
     <div id="timeline_icon_wrapper" class="svg_wrapper">
-      <img id="about_icon"src="https://frog.dianaband.info/src/img/icon_timeline.svg" alt="구름 모양 타임라인 버튼">
+      <img id="about_icon"src="https://frog2.dianaband.info/src/img/icon_timeline.svg" alt="구름 모양 타임라인 버튼">
     </div>
 
   <span id="to_about">
