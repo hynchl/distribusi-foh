0c35b9d4307bb2008649c2259a81b22e0a41e8fc
Dooho Yi <pilotedeguerre@gmail.com>
2020-11-29 08:22:20 +0000

    deploy.sh added

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,39 @@
-# HELP 도움이 필요해요 HELP
-# 깃허브에서 이 repository를 clone해서
-# 그걸 바로 재설치하면 될 것 같아서 구현 예정!
+#!/bin/bash
+
+echo "---> 'git fetch'"
+git fetch
+echo "---> 'git pull'"
+git pull
+
+echo "---> 'rm data/.ignore'"
+rm data/.ignore
+echo "---> 'cp test_data/.ignore data/'"
+cp test_data/.ignore data/
+
+echo "---> 'rm -r data/src'"
+rm -r data/src
+echo "---> 'cp -r test_data/src/ data/'"
+cp -r test_data/src/ data/
+
+echo "---> 'rm data/about.html'"
+rm data/about.html
+echo "---> 'cp test_data/about.html data/'"
+cp test_data/about.html data/
+
+echo "---> 'rm data/home.html'"
+rm data/home.html
+echo "---> 'cp test_data/home.html data/'"
+cp test_data/home.html data/
+
+echo "---> 'rm data/participants.html'"
+rm data/participants.html
+echo "---> 'cp test_data/participants.html data/'"
+cp test_data/participants.html data/
+
+echo "---> 'rm data/index.html'"
+rm data/index.html
+echo "---> 'cp test_data/index.html data/'"
+cp test_data/index.html data/
+
+echo "---> 'python distribusi/test.py -d ./data/'"
+python distribusi/test.py -d ./data/
