458b630347ecc41e18c8613dcecd0411153b4dc7
Dooho Yi <pilotedeguerre@gmail.com>
2021-02-05 14:42:19 +0900

    ..

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 
-echo "---> 'git fetch'"
+#monitoring each command invokations (on)
+set -x
+
 git fetch
-echo "---> 'git pull'"
 git pull
 
-echo "---> 'rm data/.ignore'"
 rm data/.ignore
-echo "---> 'cp test_data/.ignore data/'"
 cp test_data/.ignore data/
 
-echo "---> 'rm -r data/src'"
 rm -r data/src
-echo "---> 'cp -r test_data/src/ data/'"
 cp -r test_data/src/ data/
 
-echo "---> 'rm data/about.html'"
 rm data/about.html
-echo "---> 'cp test_data/about.html data/'"
 cp test_data/about.html data/
 
-echo "---> 'rm data/home.html'"
 rm data/home.html
-echo "---> 'cp test_data/home.html data/'"
 cp test_data/home.html data/
 
-echo "---> 'rm data/participants.html'"
 rm data/participants.html
-echo "---> 'cp test_data/participants.html data/'"
 cp test_data/participants.html data/
 
-echo "---> 'rm data/index.html'"
 rm data/index.html
-echo "---> 'cp test_data/index.html data/'"
 cp test_data/index.html data/
 
-echo "---> 'python distribusi/test.py -t -d ./data/'"
 python distribusi/test.py -t -d ./data/
+
+#monitoring each command invokations (off)
+set +x
