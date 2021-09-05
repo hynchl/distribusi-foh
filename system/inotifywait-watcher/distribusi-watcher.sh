1489368d205f26ace0134f2f8fe695d4e17f1e70
Dooho Yi <pilotedeguerre@gmail.com>
2021-09-05 14:41:59 +0000

    update distribusi-watcher.sh

--- system/inotifywait-watcher/distribusi-watcher.sh
+++ system/inotifywait-watcher/distribusi-watcher.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:$HOME/.local/bin
 export PYENV_ROOT="/home/nextcloud/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
 fi
 eval "$(pyenv virtualenv-init -)"
 
