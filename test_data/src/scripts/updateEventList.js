27e21be5a39831413d9bad70d7c974994e77f176
Dooho Yi <pilotedeguerre@gmail.com>
2021-11-23 12:33:21 +0000

    sorting by 'number' @ TIMELINE page

--- test_data/src/scripts/updateEventList.js
+++ test_data/src/scripts/updateEventList.js
@@ -13,26 +13,56 @@ ed_resquest.onload = function() {
   event_list = ed_resquest.response;
 
   console.log(event_list)
-  for (let key in event_list){
-    console.log(key);
+  // for (let key in event_list){
+  //   console.log(key);
 
-    // convert date format( `.` -> `/` )
-    let event = event_list[key];
-    let date = event.date.split('.');
-    date.pop();
-    let date_str = date.join(' /');
-    
-    let row = document.createElement('div');
-    row.classList.add("event_row")
-    row.innerHTML = `${date_str} &emsp;&emsp; #${key} ${event.title}`
-    let link = document.createElement('a');
-    link.href = `${key}/`
-    link.appendChild(row)
-    
-    document.getElementById("about_wrapper").appendChild(link);
-    // TODO: add hover effect
-    // TODO: add 
-  }
+  //   // convert date format( `.` -> `/` )
+  //   let event = event_list[key];
+  //   let date = event.date.split('.');
+  //   date.pop();
+  //   let date_str = date.join(' /');
+
+  //   let row = document.createElement('div');
+  //   row.classList.add("event_row")
+  //   row.innerHTML = `${date_str} &emsp;&emsp; #${key} ${event.title}`
+  //   let link = document.createElement('a');
+  //   link.href = `${key}/`
+  //   link.appendChild(row)
+
+  //   document.getElementById("about_wrapper").appendChild(link);
+  //   // TODO: add hover effect
+  //   // TODO: add
+  // }
+
+  Object.entries(event_list)
+
+    .sort(function(a, b) {
+      return a[1].number > b[1].number
+    })
+
+    .forEach(function(item) {
+      console.log(item);
+
+      //
+      let key = item[0];
+      let event = item[1];
+
+      // convert date format( `.` -> `/` )
+      let date = event.date.split('.');
+      date.pop();
+      let date_str = date.join(' /');
+
+      let row = document.createElement('div');
+      row.classList.add("event_row")
+      row.innerHTML = `${date_str} &emsp;&emsp; #${key} ${event.title}`
+      let link = document.createElement('a');
+      link.href = `${key}/`
+      link.appendChild(row)
+
+      document.getElementById("about_wrapper").appendChild(link);
+      // TODO: add hover effect
+      // TODO: add
+    })
 
 
 }
